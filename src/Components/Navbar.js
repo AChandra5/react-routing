6bc450e4ea22a60b3af0b163f2b0b94102539d8f
Akhil chandra <akhilchandra2309@gmail.com>
2023-02-09 10:25:35 +0530

    nav styles edited

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,15 +4,23 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
 
     return (
-        
-            <nav className='nav'>
-                
-                <ul style={{ display: "flex", justifyContent: "space-evenly", background: "gray", position: "fixed", top: "-16px", zIndex: "1", width: "100%", height: "40px" }}>
-                    <li style={{ padding: "5px", listStyleType: "none" }}><Link to="/">Home</Link></li>
-                    <li style={{ padding: "5px", listStyleType: "none" }}><Link to="/table">Table</Link></li>
-                </ul>
-            </nav>
-        
+
+        <nav className='nav'>
+
+            <ul style={{ display: "flex", justifyContent: "space-evenly", background: "gray", position: "fixed", top: "-16px", zIndex: "1", width: "100%", height: "40px" }}>
+                <li style={{ padding: "5px", listStyleType: "none", color: "white" }}><Link to="/" style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontWeight: "bold"
+                }} >Home</Link></li>
+                <li style={{ padding: "5px", listStyleType: "none" }}><Link to="/table" style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontWeight: "bold"
+                }}>Table</Link></li>
+            </ul>
+        </nav>
+
 
     )
 }
